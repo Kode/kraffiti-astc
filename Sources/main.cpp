117dd002a717521d631705a168649ad32cf94e00
Robert Konrad <git2016@robdangero.us>
2017-05-29 10:58:25 +0200

    Enable silent mode

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -10,6 +10,6 @@ dynfunc char* formats() {
 int astc_main(int argc, char **argv, int width, int height, int stride, int format, unsigned char* pixels, int* out_width, int* out_height, int* out_size, void** out_data);
 
 dynfunc void encode(int width, int height, int stride, int format, unsigned char* pixels, int* out_width, int* out_height, int* out_size, void** out_data) {
-	char* args[] = { "kraffiti-astc.dll", "-c" , "test.png", "test.astc", "6x6", "-medium" };
-	astc_main(6, args, width, height, stride, format, pixels, out_width, out_height, out_size, out_data);
+	char* args[] = { "kraffiti-astc.dll", "-c" , "test.png", "test.astc", "6x6", "-medium", "-silentmode" };
+	astc_main(7, args, width, height, stride, format, pixels, out_width, out_height, out_size, out_data);
 }
