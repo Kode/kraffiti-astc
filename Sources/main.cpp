ea11552e6d4d6346267e4b2699cb119252c60a52
Robert Konrad <git2016@robdangero.us>
2017-05-29 18:53:13 +0200

    Fix encode function signature

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -4,12 +4,12 @@
 #define dynfunc extern "C" __declspec(dllexport)
 
 dynfunc char* formats() {
-	return "astc";
+	return "ASTC";
 }
 
-int astc_main(int argc, char **argv, int width, int height, int stride, int format, unsigned char* pixels, int* out_width, int* out_height, int* out_size, void** out_data);
+int astc_main(int argc, char **argv, int width, int height, int stride, const char* format, unsigned char* pixels, int* out_width, int* out_height, int* out_size, void** out_data);
 
-dynfunc void encode(int width, int height, int stride, int format, unsigned char* pixels, int* out_width, int* out_height, int* out_size, void** out_data) {
+dynfunc void encode(int width, int height, int stride, const char* format, unsigned char* pixels, int* out_width, int* out_height, int* out_size, void** out_data) {
 	char* args[] = { "kraffiti-astc.dll", "-c" , "test.png", "test.astc", "6x6", "-medium", "-silentmode" };
 	astc_main(7, args, width, height, stride, format, pixels, out_width, out_height, out_size, out_data);
 }
